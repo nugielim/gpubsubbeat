8b60eccf189c49660496dbec56afe3fba779180d
Dejan Golja <dejan@elastic.co>
2017-07-28 17:13:07 +0200

    This PR change gpubsubbeat in that way that is possible to run multiple beats in the same GCP project account, since the topic and subscriber names are not hard coded anymore.
    To do that I introduced few new variables:
    * topic_name - name on the topic to subscribe to
    * create_topic - If topic doesn't exist create the topic
    * subscriber_name - subscriber name. If it doens't exist create one
    * subscriber_prefix - randomly generated subscriber name with the predefined prefix.
    
    Improvements:
    * Improve the message error if the subscriber already exists
    * Verify if the subscriber pulls the data from the right topic
    * minor structure code changes to simplify testing
    * code formatting
    * safety check for the payload, so if not well formatted the message get ignored
    
    TODO:
    * further code refactor to improve code testing
    * refactor signal handling
    * remove unused tickers

--- config/config.go
+++ config/config.go
@@ -6,14 +6,17 @@ package config
 import "time"
 
 type Config struct {
-	Period      time.Duration `config:"period"`
-	ProjectID   string        `config:"projectid"`
-	MaxFetchMsg int           `config:"maxfetchmsg"`
-	Region      string        `config:"region"`
+	Period           time.Duration `config:"period"`
+	ProjectID        string        `config:"projectid"`
+	TopicName        string        `config:"topic_name"`
+	CreateTopic      bool          `config:"create_topic"`
+	SubscriberName   string        `config:"subscriber_name"`
+	SubscriberPrefix string        `config:"subscriber_prefix"`
 }
 
 var DefaultConfig = Config{
-	Period:      1 * time.Second,
-	ProjectID:   "env",
-	Region:      "us-central1",
+	Period:         1 * time.Second,
+	ProjectID:      "env",
+	TopicName:      "gpubsubbeat-all",
+	SubscriberName: "gpubsubbeat-subscription",
 }
